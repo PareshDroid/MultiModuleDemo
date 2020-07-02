1e4dcdde9b88dce9286b7499459ca82cab58eede
PareshDroid <pareshandroiderrors@gmail.com>
2020-07-02 21:27:43 +0530

    buildsrc added for multimodule

---
+++ buildSrc/src/main/kotlin/TestDependencies.kt
@@ -0,0 +1,10 @@
+
+
+object TestDependencies {
+
+    const val JUNIT = "junit:junit:4.12"
+    const val EXT_JUNIT = "androidx.test.ext:junit:1.1.1"
+    const val ESPRESSO = "androidx.test.espresso:espresso-core:3.2.0"
+
+
+}
